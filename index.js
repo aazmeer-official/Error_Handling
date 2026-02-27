97c8fb2e750d495f38e4d48bc2c246f49b9ce4fa
Muhammad Aazmeer <aazmeer.official@gmail.com>
2026-02-27 16:33:08 +0500

    Errors Handled

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const ExpressError = require("./ExpressError")
 
 
+const checkToken = (req,res,next)=>{
+    let {token} = req.query;
+    if(token === "access"){
+        next();
+    }else{
+    console.log("Check Token")
+    throw new ExpressError(401,"ACCESS NHI DENI")
+    }
+}
 
+app.get("/api",checkToken,(req,res)=>{
+    res.send("Dsata")
+})
 
 app.get("/",(req,res)=>{
     res.send("Response")
@@ -18,7 +31,8 @@ app.get("/error",(req,res)=>{
 
 app.use((err,req,res,next)=>{
     console.log("------------ERROR-------------")
-    next(err)
+    let {status=500,message="Some Error Occurred"} = err;
+    res.status(status).send(message)
 })
 
 app.listen(port,()=>{
